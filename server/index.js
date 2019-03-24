ac55cca8ce1c1095cd269987e2d3f40fc5d54125
Robert Checco <robert.checco@gmail.com>
2019-03-23 21:07:17 -0400

    Minimum configuration for google strategy

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: 'auth/google/callback'
+        },
+        (accessToken) => {
+            console.log(accessToken);
+        }
+    )
+);
 
 app.get('/', (req, res) => {
     res.send('<h1>Project Glutenberg</h1>');
@@ -14,6 +26,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Application is running on Port ${PORT}`);
 });
-
-clientid: 652684636409-mmsbb060cn9gkifstrjobqg0ci4i09qu.apps.googleusercontent.com
-secret: uKpu_SclnL7Eigv-vmoa_RAs

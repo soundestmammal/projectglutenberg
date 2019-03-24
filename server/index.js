07b4b15f1034cc808b1ab8f7a3871001b8df2ceb
Robert Checco <robert.checco@gmail.com>
2019-03-23 21:01:30 -0400

    Add Passport and Google Strategy to Project

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
+
+passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
     res.send('<h1>Project Glutenberg</h1>');
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Application is running on Port ${PORT}`);
+});
+
+clientid: 652684636409-mmsbb060cn9gkifstrjobqg0ci4i09qu.apps.googleusercontent.com
+secret: uKpu_SclnL7Eigv-vmoa_RAs

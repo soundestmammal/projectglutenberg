838846a3aa6d8d75f3fdfd7244802f5790fcc7f3
Robert Checco <robert.checco@gmail.com>
2019-03-27 23:53:47 -0400

    Enable cookie based authentication and nudge user along auth flow

---
+++ client/src/components/google.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Google extends Component{
+    state = {isSignedIn: false}
+
+    isSignedIn = () => {
+        if (!isSignedIn) {
+            
+        }
+    }
+    render() {
+        
+    }
+}
+
+export default Google;
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user=> {
+        done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy(
@@ -10,9 +23,35 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
-            console.log('access token', accessToken);
-            console.log('refresh token', refreshToken);
-            console.log('profile:', profile);
+            User.findOne( {googleId: profile.id }).then((existingUser) => {
+                if (existingUser) {
+                    // What is the deal with this done? Is it a passport js thing?
+                    done(null, existingUser);
+                    console.log("This is an existing user");
+                } else {
+                    new User({ googleId: profile.id })
+                    .save()
+                    .then(user => {
+                        done(null, user);
+                    })
+                }
+            });
         }
     )
 );
+
+/* ?? What is the next step ??
+
+So far... we either make a new record or retrieve a new record.
+
+We need to generate an identifying information in a cookie (Kinda Complicated)
+
+1a) Log me in please
+1b) You appear to be user_xyz let me give you a token
+        call serializeUser(user) to generate the token and put it in the cookie
+2a) Client makes a request (gives the cookie in request)
+2b) Takes the cookie to deserialize the user. Identify the user.
+        Ah. It appears to be userxyz, let's give them what they are looking for.
+
+
+*/ 

97f09fa32ec6204479db9f32bf7f019c097b3171
Rob <robert.checco@gmail.com>
2020-02-16 18:55:04 -0800

    Create a /signup route that posts to the server

--- server/router.js
+++ server/router.js
@@ -1,5 +1,5 @@
+const Authentication = require('./controllers/authentication');
+
 module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.send("Project Glutenberg");
-    });
+    app.post('/signup', Authentication.signup );
 }

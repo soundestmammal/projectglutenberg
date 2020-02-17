b9f5da520a739d18a8139d0273035c8265ec4a80
Rob <robert.checco@gmail.com>
2020-02-16 18:54:44 -0800

    Require that the email field be unique

--- server/models/User.js
+++ server/models/User.js
@@ -3,13 +3,13 @@ const Schema = mongoose.Schema;
 
 // Define the User Model
 const userSchema = new Schema({
-    email: String,
+    email: { type: String, lowercase: true, unique: true }, 
     password: String
 });
 
 // Create the model class
-
+const ModelClass = mongoose.model('user', userSchema);
 
 // Export the model
-
+module.exports = ModelClass;
 

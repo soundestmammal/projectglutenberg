a1e3cb801897e9594218e6b83470383f3f3ea5a6
Rob <robert.checco@gmail.com>
2020-02-28 13:42:54 -0800

    Refactor out a file for router

--- server/router.js
+++ server/router.js
@@ -1,5 +1,14 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
 
-module.exports = (app) => {
+// require auth, is the helper function to "hey are you authenticated"
+const requireAuth = passport.authenticate('jwt', { session: false });
+
+module.exports = function (app) {
+    // requireAuth is basically middleware?
+    app.get('/', requireAuth, (req, res) => {
+        res.send({ hey: "there" });
+    });
     app.post('/signup', Authentication.signup );
 }

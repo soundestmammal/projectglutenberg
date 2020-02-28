3e7dcb3d545f2fdebaa35bebdfe25a1fe750364a
Rob <robert.checco@gmail.com>
2020-02-28 13:44:13 -0800

    Authenticating users

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,11 +1,23 @@
+const jwt = require('jwt-simple');
+const key = require('../config/keys');
 const User = require('../models/user');
+
+const tokenForUser = (user) => {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, key.secret);
+}
 
 exports.signup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
 
     // See if a user with a given email exists...
-    User.findOne({ email: email}, async (err, existingUser) => {
+    User.findOne({ email: email}, (err, existingUser) => {
+
+        if (!email || !password) {
+            return res.status(422).send({ error: 'You must provide a email and a password!!!'});
+        }
+
         if (err) { return next(err); }
         
         // If a user with the email does exists
@@ -19,10 +31,7 @@ exports.signup = (req, res, next) => {
         user.save(function(err) {
             if (err) { return next(err); }
             // Respond to incoming request
-            res.json(user);
+            res.json({ token: tokenForUser(user) });
         });
-    })
-
-
-
+    });
 }

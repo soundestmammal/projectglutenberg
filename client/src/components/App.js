f0099fa23d4539ef82977a0dc2525e3a4c0a7159
Rob <robert.checco@gmail.com>
2020-03-06 18:28:44 -0800

    Entire app is now enclosed in the router tag

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 import Header from './Header';
 import List from './List';
 import Map from './Map';
 import "../styles/app.css";
 
+/*
+  I am using React Router Dom to help with my navigation around my application.
+  I wrapped my entire application in a Router tag so the entire application is right here. If I need to nest any routes then I can do that right here.
+*/
+
 const App = () => {
     return (
-        <div style={{margin: 0, padding: 0}}>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/product">Product</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/product">
+          <div style={{margin: 0, padding: 0}}>
             <Header />
             <div className="map-list-wrapper">
                 <List />
@@ -15,8 +39,17 @@ const App = () => {
                     onIdle
                 />
             </div>
-            
         </div>
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+         
+      </Router>
     );
 }
 

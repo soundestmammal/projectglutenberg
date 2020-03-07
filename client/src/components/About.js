fa253d06b976e6c6d5108d901a1d6baab6411fe5
Rob <robert.checco@gmail.com>
2020-03-06 18:29:12 -0800

    Add Home and About components to the project

---
+++ client/src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div>Hey this is where you can learn more about project glutenberg</div>
+    )
+}
+
+export default About;
---
+++ client/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div>This is the home page welcome to project glutenberg</div>
+    )
+}
+
+export default Home;

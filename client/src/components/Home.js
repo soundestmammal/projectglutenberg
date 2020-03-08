37a79b967fb9a4ed41ec9c089c6a287d9a269c47
Rob <robert.checco@gmail.com>
2020-03-08 11:28:40 -0700

    Add a home page and have it display a group picture

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import '../styles/home.css';
 
 const Home = () => {
     return(
-        <div>This is the home page welcome to project glutenberg</div>
+        <div>
+            <h1>Project Glutenberg</h1>
+            <img src="https://images.unsplash.com/photo-1531206715517-5c0ba140b2b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="group-image" alt="Group of people together" />
+        </div>
     )
 }
 

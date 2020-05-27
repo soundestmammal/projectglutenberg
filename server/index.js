1b5fde866950d3f875bfb6ead0829431521ac67f
Rob <robert.checco@gmail.com>
2020-05-26 19:03:31 -0700

    Server-side refactor

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const app = express();
-const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
+
+const router = require('./router');
 // Connect to the database
 require('./config/db');
 
@@ -17,7 +20,6 @@ router(app);
 // Server Setup
 const PORT = process.env.PORT || 3090;
 const server = http.createServer(app); // library for working with http requests.
-
 
 server.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}`);

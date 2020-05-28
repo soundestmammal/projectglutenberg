9073b43ea1241cdf695b7ddb5aaf09a80457aa71
Rob <robert.checco@gmail.com>
2020-05-28 12:51:27 -0700

    User interface micro improvements

---
+++ client/src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/button.css";
+
+const Button = (props) => {
+    return(
+        <Link style={{textDecoration: 'none'}}>
+            <div className="button">
+                {props.text}
+            </div>
+        </Link>
+    );
+}
+
+export default Button;
---
+++ client/src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/logo.css";
+
+const Logo = () => {
+    return(
+        <div className="logo">
+            <span>Project Glutenberg</span>
+        </div>
+    )
+}
+
+export default Logo;

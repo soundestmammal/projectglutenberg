a091378d835b7a2fa287868fbeebb779397b7d98
Rob <robert.checco@gmail.com>
2020-05-30 16:12:58 -0700

    Add redux-thunk to project and add it as a middleware

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(reduxThunk)
+);
+
 ReactDOM.render(
-  <Provider store={createStore(reducers, {})}>
+  <Provider store={store}>
     <Router>
       <App>
 

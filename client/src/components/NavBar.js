6f2e159f16b2072233d33eb3c70d69cece1a06b4
Rob <robert.checco@gmail.com>
2020-05-31 17:18:29 -0700

    Conditionally render the NavBar component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,30 @@
 import React from 'react';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Logo from './Logo';
 import Button from './Button';
 import '../styles/navbar.css';
 
 const NavBar = (props) => {
+    if (props.auth) {
+        return(
+            <nav className="nav-container">
+                <div className="navigation">
+                    <Link to="/" style={{textDecoration: 'none'}}><Logo /></Link>
+                    <form className="search-container" onSubmit={props.submit}>
+                        <label className="search-label">
+                            <input className="nav-searchbar" type='text' value={props.value} onChange={props.change}></input>
+                            <input className="nav-searchbar" type='text' value={props.location} onChange={props.changeLocation}></input>
+                        </label>
+                            <input className="nav-search-button" type='submit' value="Go"></input>
+                    </form>
+                    {/* <Link to="/auth" className="signin" style={{textDecoration: 'none'}} >Sign in / Sign up</Link> */}
+                    <Button className="nav-auth-button" text="Sign out" dest="/signout" />
+                </div>
+            </nav>
+        );
+    } 
     return(
         <nav className="nav-container">
             <div className="navigation">
@@ -25,4 +44,8 @@ const NavBar = (props) => {
     );
 }
 
-export default NavBar;
+function mapStateToProps(state) {
+    return { auth: state.auth.authenticated }
+}
+
+export default connect(mapStateToProps)(NavBar);

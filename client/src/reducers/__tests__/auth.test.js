d22e8dd3bf7690aa2b448f362bb8dfee8cddc4c2
Rob <robert.checco@gmail.com>
2020-06-09 13:27:06 -0700

    client-testing-04 - authReducer handles actions of type AUTH_USER

--- client/src/reducers/__tests__/auth.test.js
+++ client/src/reducers/__tests__/auth.test.js
@@ -1,5 +1,5 @@
 import authReducer from '../auth';
-import { FETCH_USER } from '../../actions/types';
+import { AUTH_USER, AUTH_ERROR, AUTH_UUID, FETCH_USER, SIGN_OUT } from '../../actions/types';
 
 const INITIAL_STATE = {
     authenticated: '',
@@ -8,14 +8,17 @@ const INITIAL_STATE = {
     user: {}
 }
 
+it('handles actions of type AUTH_USER', () => {
+    const token = "jfdnsaofenwonfeoiwhfhf98h298hf9h4938hf4398hf3";
+    const action = { type: AUTH_USER, payload: token };
+    const newState = authReducer(INITIAL_STATE, action);
+    expect(newState.authenticated).toEqual(token);
+});
+
 it('handles actions of type FETCH_USER', () => {
 
     const user = { uuid: 'fidwqn09ej0', avatar: []};
-    const action = {
-        type: FETCH_USER,
-        payload: user
-    }
-
+    const action = { type: FETCH_USER, payload: user }
     const newState = authReducer(INITIAL_STATE, action);
     expect(newState.user).toEqual(user);
 });

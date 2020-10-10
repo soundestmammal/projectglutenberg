e3a93fe4f8f7775b455a88646b06b5f2b214657c
Rob <robert.checco@gmail.com>
2020-10-10 14:51:04 -0400

    Refactor index and app for easier testing

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const auth = require("./middleware/auth");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// Connect to the database
+require("./config/db");
+
+app.set("trust proxy", "127.0.0.1");
+
+const userRouter = require("./router");
+const adminRouter = require("./admin/admin");
+
+// App Setup
+app.use(morgan("combined")); // Middleware to log out requests
+app.use(bodyParser.json()); // Middleware to ???
+app.use(userRouter);
+app.use(adminRouter);
+
+module.exports = app;

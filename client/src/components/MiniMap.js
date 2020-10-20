c57f74aa1d3465e3f433f49e746c2267a051fba3
Rob <robert.checco@gmail.com>
2020-10-19 21:56:26 -0400

    Enable access to secure key from NewMap and MiniMap

--- client/src/components/MiniMap.js
+++ client/src/components/MiniMap.js
@@ -1,36 +1,38 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import MiniMarker from './MiniMarker';
-import { key } from '../KEYS';
-import "../styles/map.css";
-import "../styles/minimap.css";
+import { googleMapsApiKey } from '../test';
+import '../styles/map.css';
+import '../styles/minimap.css';
 
 class MiniMap extends Component {
+    static defaultProps = {
+        zoom: 15,
+    };
 
-  static defaultProps = {
-    zoom: 15
-  };
+    renderMap = () => {
+        return (
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: googleMapsApiKey }}
+                center={this.props.center}
+                zoom={this.props.zoom}
+            >
+                <MiniMarker
+                    lat={this.props.center.lat}
+                    lng={this.props.center.lng}
+                />
+            </GoogleMapReact>
+        );
+    };
 
-  renderMap = () => {
-    return (
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: key}}
-        center={this.props.center}
-        zoom={this.props.zoom}
-      >
-        <MiniMarker lat={this.props.center.lat} lng={this.props.center.lng} />
-      </GoogleMapReact>
-    );
-  }
-
-  render() {
-    return (
-      <div className="minimap-container">
-        {this.renderMap()}
-        <div className="map-overlay"></div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='minimap-container'>
+                {this.renderMap()}
+                <div className='map-overlay'></div>
+            </div>
+        );
+    }
 }
 
 export default MiniMap;
--- client/src/components/NewMap.js
+++ client/src/components/NewMap.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import MapCheckbox from './MapCheckbox';
 import Marker from './Marker';
-import { googleMapsApiKey } from '../test.js';
+import { googleMapsApiKey } from '../test';
 import '../styles/map.css';
 
 /*

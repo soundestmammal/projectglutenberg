319912530983d8ab088a1407b1920aa24931850b
Rob <robert.checco@gmail.com>
2020-10-26 18:06:28 -0400

    Add circle ci configuration file

---
+++ server/env-keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    yelp: process.env.YELP_API_KEY,
+    mongoDBConnection: process.env.MONGO_DB_CONNECTION,
+    jwtSecret: process.env.JWT_SECRET,
+    openCage: process.env.OPEN_CAGE_API_KEY,
+    ipGeolocation: process.env.IP_GEOLOCATION_API_KEY
+}
